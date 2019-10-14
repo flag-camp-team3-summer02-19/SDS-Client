28a5ac95f225550452fa5d1d39018feefbb141a8
yangzgnay <yangzgnay@gmail.com>
2019-10-13 22:20:24 -0700

    add UserPanel

--- sds-client/src/components/DashBoard.js
+++ sds-client/src/components/DashBoard.js
@@ -1,23 +1,19 @@
 import React, {Component} from 'react';
 import history from '../history';
+import UserPanel from "./UserPanel";
 
-/* TODO 1: add <UserPanel /> in control-panel */
 /* TODO 3: add <SearchPanel /> in control-panel */
 /* TODO 4: add <OrderList /> under control-panel */
 class DashBoard extends Component {
     state = {
         userInfo: this.props.userInfo
-    }
+    };
 
     render() {
         return (
             <div id="dashboard">
                 <section id="control-panel">
-                    <p>
-                        this is user panel
-                        <button onClick={() => {history.push('/login')}}> Logout </button>
-                    </p>
-
+                    <UserPanel userId={this.state.userInfo.username}/>
                     <button onClick={() => {history.push('/newOrder')}}> Make New Order </button>
                     <p>this is search panel</p>
                 </section>
---
+++ sds-client/src/components/UserPanel.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react';
+import {Redirect} from 'react-router-dom';
+import {Button } from 'antd';
+import history from '../history';
+
+
+class UserPanel extends Component {
+    state = {
+        loggedIn: true
+    };
+
+    onClickLogout = () => {
+        //TODO: tell server this user is logged out
+        //TODO: destroy current session
+        this.setState({loggedIn: false});
+        // history.push('/');
+    };
+
+
+    render() {
+        return (
+            <div id="user-panel">
+                <h3>Welcome: {this.props.userId}</h3>
+                <Button onClick={this.onClickLogout} className="logout-button">Logout</Button>
+                {this.state.loggedIn ? null : <Redirect to="/login"/>}
+            </div>
+        );
+    }
+}
+
+export default UserPanel;
