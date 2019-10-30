b04f376bf7431608b8548af8350ca0605e787c6a
yangzgnay <yangzgnay@gmail.com>
2019-10-29 23:35:55 -0700

    fix the styling problem when there are too many tags. And also added a skeleton of a button to clear all filters

--- sds-client/src/components/FilterTags.js
+++ sds-client/src/components/FilterTags.js
@@ -1,11 +1,12 @@
 import React, {Component} from 'react';
-import {Tag} from "antd";
+import {Tag, Button} from "antd";
 
 class FilterTags extends Component {
     render() {
         const {tags} = this.props;
         return (
             <div className="filter-tags">
+                {tags.length > 0?<Button onClick={()=> {console.log("clear")}}>clear all</Button>:null}
                 {tags.map((currentValue, index, arr) => {
                     return (<Tag key={index}
                          closable
